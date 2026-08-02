97c0077554aa01c0958452cf622e034a35ca651d
japatan <zhafranrazz@gmail.com>
2026-08-02 23:08:47 +1000

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,16 @@
+// O(n log n)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> countS = new HashMap<>();
+        Map<Character, Integer> countT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            // .put() is to add or update key value pair
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return countS.equals(countT);
+    }
+}
